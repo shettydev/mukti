53f2ccc75282c90093abe2e282de66d3747b8369
Prathik Shetty <prathikshetty1411@gmail.com>
2025-07-21 02:57:06 +0530

    fix(@mukti/web):fix import path for utils module

--- packages/mukti-web/src/components/magicui/animated-shiny-text.tsx
+++ packages/mukti-web/src/components/magicui/animated-shiny-text.tsx
@@ -1,6 +1,5 @@
+import { cn } from "@/lib/utils";
 import { ComponentPropsWithoutRef, CSSProperties, FC } from "react";
-
-import { cn } from "/lib/utils";
 
 export interface AnimatedShinyTextProps
   extends ComponentPropsWithoutRef<"span"> {
